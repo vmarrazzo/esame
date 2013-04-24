83aac8503de6db22a6f75794ff19ac1e06eded39
Vincenzo Marrazzo <pariamentz@gmail.com>
2013-04-24 16:35:24 +0300

    Create persona.cpp

---
+++ ristorazione/persona.cpp
@@ -0,0 +1,19 @@
+#include "persona.h"
+
+namespace ristorazione {
+
+
+      Persona::Persona() {
+    
+      }
+    
+      Persona::~Persona() {
+    
+      } 
+    
+      bool Persona::operator==(Persona& rhs)const {
+    
+              return this == &rhs;
+      }
+
+}
